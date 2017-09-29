3336b65f00f3cab141011e0272de7ab56db2d46b
Prempi <realp2em@gmail.com>
2017-09-29 11:40:56 +0700

    Open window

--- cil.py
+++ cil.py
@@ -3,14 +3,14 @@ import arcade
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
  
-class SnakeWindow(arcade.Window):
+class RoomWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.BLACK)
  
 def main():
-    window = SnakeWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window = RoomWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.set_window(window)
     arcade.run()
  
